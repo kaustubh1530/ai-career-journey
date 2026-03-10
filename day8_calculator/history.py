742762980adf2db234155e8593d1c802487309a0
Kaustubh Patil <kauspatil1530@gmail.com>
2026-03-09 23:40:00 -0400

    Reorganized repo: each day in its own folder

--- day8_calculator/history.py
+++ day8_calculator/history.py
@@ -1,15 +1,38 @@
-history_file = "calc_history.txt"
+import json
 
+history_file = "calc_history.json"
 
-def save_history(text):
-    with open(history_file, "a") as file:
-        file.write(text + "\n")
+def save_history(operation, num1, num2, result):
+
+    new_entry = {
+        "operation": operation,
+        "num1": num1,
+        "num2": num2,
+        "result": result
+    }
+
+    try:
+        with open(history_file, "r") as file:
+            data = json.load(file)
+    except:
+        data = []
+
+    data.append(new_entry)
+
+    with open(history_file, "w") as file:
+        json.dump(data, file, indent=4)
 
 
 def view_history():
+
     try:
         with open(history_file, "r") as file:
-            print("\n--- Calculation History ---")
-            print(file.read())
-    except FileNotFoundError:
+            data = json.load(file)
+
+        print("\n--- Calculation History ---")
+
+        for entry in data:
+            print(f"{entry['num1']} {entry['operation']} {entry['num2']} = {entry['result']}")
+
+    except:
         print("No history found yet.")
